4b8e165d82e41a54072a693f53f2187ad8a49a68
sirenuf <10t00lbox0@disroot.org>
2026-02-26 01:43:52 +0100

    Add clantag saving capability

--- src/Games/COD/MW2.cpp
+++ src/Games/COD/MW2.cpp
@@ -14,26 +14,31 @@ namespace MW2
 		{
 			std::hash_map<std::string, MemoryEntry> m;
 
-			m.insert(std::make_pair("Prestige",		MemoryEntry{ 0x01FF9A9C, 1		}));
-			m.insert(std::make_pair("Experience",	MemoryEntry{ 0x01FF9A94, 4		}));
-			m.insert(std::make_pair("Score",		MemoryEntry{ 0x01FF9AA4, 4		}));
+			m.insert(std::make_pair("Prestige",			MemoryEntry{ 0x01FF9A9C, 1		}));
+			m.insert(std::make_pair("Experience",		MemoryEntry{ 0x01FF9A94, 4		}));
+			m.insert(std::make_pair("Score",			MemoryEntry{ 0x01FF9AA4, 4		}));
 
-			m.insert(std::make_pair("Wins",			MemoryEntry{ 0x01FF9ADC, 4		}));
-			m.insert(std::make_pair("Losses",		MemoryEntry{ 0x01FF9AE0, 4		}));
-			m.insert(std::make_pair("Ties",			MemoryEntry{ 0x01FF9AE4, 4		}));
-			m.insert(std::make_pair("WinStreak",	MemoryEntry{ 0x01FF9AE8, 4		}));
+			m.insert(std::make_pair("Wins",				MemoryEntry{ 0x01FF9ADC, 4		}));
+			m.insert(std::make_pair("Losses",			MemoryEntry{ 0x01FF9AE0, 4		}));
+			m.insert(std::make_pair("Ties",				MemoryEntry{ 0x01FF9AE4, 4		}));
+			m.insert(std::make_pair("WinStreak",		MemoryEntry{ 0x01FF9AE8, 4		}));
 
-			m.insert(std::make_pair("Kills",		MemoryEntry{ 0x01FF9AA8, 4		}));
-			m.insert(std::make_pair("HeadShots",	MemoryEntry{ 0x01FF9ABC, 4		}));
-			m.insert(std::make_pair("Assists",		MemoryEntry{ 0x01FF9AB8, 4		}));
-			m.insert(std::make_pair("Streak",		MemoryEntry{ 0x01FF9AAC, 4		}));
+			m.insert(std::make_pair("Kills",			MemoryEntry{ 0x01FF9AA8, 4		}));
+			m.insert(std::make_pair("HeadShots",		MemoryEntry{ 0x01FF9ABC, 4		}));
+			m.insert(std::make_pair("Assists",			MemoryEntry{ 0x01FF9AB8, 4		}));
+			m.insert(std::make_pair("Streak",			MemoryEntry{ 0x01FF9AAC, 4		}));
 
-			m.insert(std::make_pair("Deaths",		MemoryEntry{ 0x01FF9AB0, 4		}));
-			m.insert(std::make_pair("TimePlayed",	MemoryEntry{ 0x01FF9AC8, 12		}));
+			m.insert(std::make_pair("Deaths",			MemoryEntry{ 0x01FF9AB0, 4		}));
+			m.insert(std::make_pair("TimePlayed",		MemoryEntry{ 0x01FF9AC8, 12		}));
 
-			m.insert(std::make_pair("Accolades",	MemoryEntry{ 0x01FF9297, 440	}));
-			m.insert(std::make_pair("Unlocks",		MemoryEntry{ 0x01FFA0C7, 480	}));
-			m.insert(std::make_pair("ClassData",	MemoryEntry{ 0x01FF9E40, 640	}));
+			m.insert(std::make_pair("Accolades",		MemoryEntry{ 0x01FF9297, 440	}));
+			m.insert(std::make_pair("Unlocks",			MemoryEntry{ 0x01FFA0C7, 480	}));
+			m.insert(std::make_pair("ClassData",		MemoryEntry{ 0x01FF9E40, 640	}));
+
+			
+			m.insert(std::make_pair("ClanTag",			MemoryEntry{ 0x00A053A4, 4		}));
+			m.insert(std::make_pair("ClanTagMenu",		MemoryEntry{ 0x00A11422, 4		})); // This is the clan tag that displays on the main menu.
+			//m.insert(std::make_pair("ClanTagCardPS3",	MemoryEntry{ 0x007B4368, 4		})); // The current clan tag in the PS3 editing box. Because why not? NVM address seems to shift.
 
 			return m;
 		}
@@ -86,6 +91,7 @@ namespace MW2
 		}
 
 		bool dataSaveFileExists = CODCommon::VerifyFilesystem(MW2);
+		libpsutil::sleep(250); // Let the game load real quick
 		if (dataSaveFileExists)
 			CODCommon::LoadSavedStats(MW2);
 
